0fd594ff895e0cd5c6af2333bb3d600b0b2b4428
Atanu Pramanik <atanupramanik506@gmail.com>
2024-08-07 21:02:19 +0530

    bookmark

---
+++ src/user/dto/editUser.dto.ts
@@ -0,0 +1,17 @@
+import { Optional } from "@nestjs/common";
+import { IsEmail, IsOptional, IsString } from "class-validator";
+
+export class EditUserDto{
+
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    firstName?: string;
+
+    @IsString()
+    @IsOptional() 
+    lastName?: string;
+}
